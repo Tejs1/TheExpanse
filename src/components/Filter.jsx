77c85b973795e50b61e4f0b791357f99d76c3553
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2022-09-10 14:42:12 +0530

    Added manufacturer  radio inputs

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { data } from "../Utils/data";
 import TwoRangeSlider from "./TwoWaySlider";
+import { getCategories } from "../Utils/categories";
 const list = data.data;
 
 export default function Filter() {
   const [rangeMinVal, setRangeMinVal] = useState(0);
   const [rangeMaxVal, setRangeMaxVal] = useState(125000);
   function rangeHandler(min, max) {
-    console.log(filterByMassRange(min, max, list));
+    const filterdShipsByRange = filterByMassRange(min, max, list);
   }
 
   const filterByMassRange = (min, max, products) => {
@@ -23,6 +24,8 @@ export default function Filter() {
         .map((ship) => ship.name);
     }
   };
+  const manufacturesList = getCategories();
+  console.log(manufacturesList);
   return (
     <div className="filter">
       <section className="filter-bar">
@@ -30,35 +33,33 @@ export default function Filter() {
           <h3>Filters</h3>
           <button>Clear</button>
         </div>
-        <div className="filter-comp">
-          <div className="filter-mass">
-            <h3 className="filter-heading">Mass [KG]</h3>
-            <div className="mass-wrapper">
-              <div className="range-list">
-                <TwoRangeSlider
-                  min={0}
-                  max={125000}
-                  minVal={rangeMinVal}
-                  maxVal={rangeMaxVal}
-                  setMaxVal={setRangeMaxVal}
-                  setMinVal={setRangeMinVal}
-                  onChange={({ min, max }) => {
-                    rangeHandler(min, max);
-                  }}
-                />
-              </div>
-              <div className="weight-list">
-                <label>over</label>
-                <label>100,000</label>
-                <label>75,000</label>
-                <label>50,000</label>
-                <label>25,000</label>
-                <label>below</label>
-              </div>
+        <div className="filter-comp mass">
+          <h3 className="filter-heading">Mass [KG]</h3>
+          <div className="mass-wrapper">
+            <div className="range-list">
+              <TwoRangeSlider
+                min={0}
+                max={125000}
+                minVal={rangeMinVal}
+                maxVal={rangeMaxVal}
+                setMaxVal={setRangeMaxVal}
+                setMinVal={setRangeMinVal}
+                onChange={({ min, max }) => {
+                  rangeHandler(min, max);
+                }}
+              />
+            </div>
+            <div className="weight-list">
+              <label>over</label>
+              <label>100,000</label>
+              <label>75,000</label>
+              <label>50,000</label>
+              <label>25,000</label>
+              <label>below</label>
             </div>
           </div>
         </div>
-        <div className="filter-comp">
+        <div className="filter-comp type">
           <h3 className="filter-heading">classification</h3>
           <ul className="list">
             <li>
@@ -75,24 +76,20 @@ export default function Filter() {
             </li>
           </ul>
         </div>
-        <div className="filter-comp">
+        <div className="filter-comp manufacturer">
           <h3 className="filter-heading">Manufacturers</h3>
           <ul className="list">
-            <li>
-              <input type="radio" name="rate" id="ANVIL" />
-              <label htmlFor="ANVIL">Anvil Aerospace</label>
-            </li>
-            <li>
-              <input type="radio" name="rate" id="BANU" />
-              <label htmlFor="BANU">BANU</label>
-            </li>
-            <li>
-              <input type="radio" name="rate" id="DRAKE" />
-              <label htmlFor="DRAKE">Drake Interplanetary</label>
-            </li>
+            {manufacturesList.map((manufacture_name) => (
+              <li key={manufacture_name}>
+                <input type="radio" name="rate" id={manufacture_name} />
+                <label htmlFor={manufacture_name}>
+                  {manufacture_name.toUpperCase()}
+                </label>
+              </li>
+            ))}
           </ul>
         </div>
-        <div className="filter-comp">
+        <div className="filter-comp price">
           <h3 className="filter-heading">PRICE RANGE</h3>
           <ul className="list">
             <li>
